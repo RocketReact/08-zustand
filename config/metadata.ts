df274d6f1d4259658938d01dd9e1ee4eea308594
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-31 16:40:24 +0300

    fix main domain

--- config/metadata.ts
+++ config/metadata.ts
@@ -1,6 +1,5 @@
 export const SITE_NAME = "NoteHub";
-export const SITE_DOMAIN =
-  "https://08-zustand-60qpd00xm-shapoval-olegs-projects.vercel.app/";
+export const SITE_DOMAIN = "https://08-zustand-beta-seven.vercel.app/";
 export const OPEN_GRAPH_IMAGE = {
   url: `${SITE_DOMAIN}/notehub-og-meta.jpg`,
   width: 1200,
