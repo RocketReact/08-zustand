5b020ec5d1675df23e5f0ef063aafa76ecc9dc95
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-02 13:14:47 +0300

    Metadata for create note - page.tsx

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,5 +1,26 @@
 import CreateNoteClient from "./CreateNote.client";
+import { HydrationBoundary } from "@tanstack/react-query";
+import type { Metadata } from "next";
+import { OG_IMAGE, SITE_DOMAIN } from "@/config/metadata";
+
+export const metadata: Metadata = {
+  title: "Create New Note",
+  description:
+    "Create a new note with title, content & tags - to organize your workflow",
+  openGraph: {
+    title: "Create New Note on a NoteNub",
+    description:
+      "Create a new note with title, content & tags - to organize your workflow",
+    url: SITE_DOMAIN,
+    images: [OG_IMAGE],
+    type: "website",
+  },
+};
 
 export default function CreateNotePage() {
-  return <CreateNoteClient />;
+  return (
+    <HydrationBoundary state={undefined}>
+      <CreateNoteClient />
+    </HydrationBoundary>
+  );
 }
