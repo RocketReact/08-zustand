9d83dc714419f612a42bf70c3ee49bae5e6ea8c7
Oleg Shapoval <olegkazhan@gmail.com>
2025-09-02 14:01:25 +0300

    delete unnecessary CreateNote.client.tsx

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -1,7 +1,8 @@
-import CreateNoteClient from "./CreateNote.client";
 import { HydrationBoundary } from "@tanstack/react-query";
 import type { Metadata } from "next";
 import { OG_IMAGE, SITE_DOMAIN } from "@/config/metadata";
+import css from "./CreateNotePage.module.css";
+import NoteForm from "@/components/NoteForm/NoteForm";
 
 export const metadata: Metadata = {
   title: "Create New Note",
@@ -20,7 +21,12 @@ export const metadata: Metadata = {
 export default function CreateNotePage() {
   return (
     <HydrationBoundary state={undefined}>
-      <CreateNoteClient />
+      <main className={css.main}>
+        <div className={css.container}>
+          <h1 className={css.title}>Create note</h1>
+          <NoteForm />
+        </div>
+      </main>
     </HydrationBoundary>
   );
 }
